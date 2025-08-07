1300ea74749c51f42d94b292bf4a77261e4d3a5c
mintbanshee <alexandria.mcqueen@triosstudent.com>
2025-08-07 13:48:34 -0400

    Added some print statements

--- utility/Ink.java
+++ utility/Ink.java
@@ -35,6 +35,59 @@ public class Ink {
   } // close goodbye
 
   public void printStats(Defender player, Weapon weapon, Armour armour, Hollowborn enemy) {
-    
+    System.out.println("*************************************");
+    System.out.printf("~~~" + GREEN + "%s(%s) Stats:%n", "Player" + RESET, player.getArchetype());
+    System.out.printf("~~~" + CYAN + "Armour:" + RESET + " %s%n", armour.getType());
+    System.out.printf("~~~" + CYAN + "Weapon:" + RESET + " %s%n", weapon.getType());
+    System.out.printf("~~~" + CYAN + "Ability:" + RESET + " %s%n", player.getSpecialAbilityName());
+    System.out.printf("~~~" + CYAN + "Health:" + RESET + " %s%n", player.getHealth());
+    System.out.printf("~~~" + CYAN + "Strength:" + RESET + " %s%n", player.getStrength());
+    System.out.printf("~~~" + CYAN + "Accuracy:" + RESET + " %s%n", player.getAccuracy());
+    System.out.println("*************************************\n");
+    System.out.printf("~~~" + RED + "%s(%s) Stats:%n", "Enemy" + RESET, enemy.getArchetype());
+    System.out.printf("~~~" + YELLOW + "Attack:" + RESET + " %s%n", enemy.getHollowbornAttack());
+    System.out.printf("~~~" + YELLOW + "Health:" + RESET + " %s%n", player.getHealth());
+    System.out.printf("~~~" + YELLOW + "Strength:" + RESET + " %s%n", player.getStrength());
+    System.out.printf("~~~" + YELLOW + "Accuracy:" + RESET + " %s%n", player.getAccuracy());
+    System.out.println("*************************************\n");
+  } // close printStats()
+
+  public void printResults(String who, int damage, Defender defender) {
+    System.out.println("*************************************");
+    if(who.equals("Player")) {
+      if(damage > 0) {
+        System.out.printf("~~~ %s does %d damage leaving%n~~~ Hollowborn with %d HP left%n",
+          who, damage, defender.getHealth());
+      }
+      else {
+        System.out.printf("~~~ %s misses, Hollowborn still has %d%n",
+          who, defender.getHealth());
+      }
+    }
+    else {
+      if(damage > 0) {
+        System.out.printf("~~~ %s does %d damage leaving%n~~~ defender with %d HP left%n",
+          who, damage, defender.getHealth());
+      }
+      else {
+        System.out.printf("~~~ %s misses, defender still has %d HP%n",
+          who, defender.getHealth());
+      }
+    }
+    System.out.println("*************************************\n");
   }
+
+  public void attackResult(float damage, Defender defender, String who) {
+    if(damage > 0) {
+      System.out.printf(GREEN + "%s %s HITS for %.0f damage!%n",
+        who, defender.getArchetype(), damage);
+      System.out.printf("%.0f HP lost%n", damage);
+      System.out.printf("%d HP remaining%n" + RESET, defender.getHealth());
+    }
+    else {
+      System.out.printf(RED + "%n%s %s MISSES!%n%n" + RESET, who, defender.getArchetype());
+    } 
+  } // close attackResult()
+
+  
 }
